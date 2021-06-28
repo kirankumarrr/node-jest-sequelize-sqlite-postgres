0982f84a7883eaa0ceba75875b3baaf0006490d0
kirankumarrr <kirankumarrrreddy@gmail.com>
2021-06-28 23:14:14 +0530

    added migration files

--- README.md
+++ README.md
@@ -6,3 +6,23 @@
 
 # Testing mails :Ethereal
   https://ethereal.email/
+
+# Testing mails :Ethereal
+
+# CREATE MIGREATE FILE 
+npx sequelize-cli migration:generate --name user-inactive-column
+
+# CREATE FIELDS WITH TABLE :
+npx sequelize-cli model:generate --name user --attributes username:string,emai:string,password:string
+
+# RUN MIGRATE
+  npx sequelize-cli db:migrate
+
+# DELETE ALL 
+  npx sequelize-cli db:migrate:undo
+  npx sequelize-cli db:migrate:undo:all
+
+# CREATE SEADER : BULK INSERT | DELETE
+  npx sequelize-cli seed:generate --name add-users
+
+  npx sequelize-cli db:seed:all
--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-const app = require('./src/app');
-const bcrypt = require("bcrypt");
-const sequelize = require('./src/config/database');
-const User = require('./src/models/User');
+const app = require("./src/app");
+const sequelize = require("./src/config/database");
 const TokenService = require("./src/services/Token");
-const addUsers = async (activeUserCount=0, inActiveUserCount=0) => {
-
-  const hash = await bcrypt.hash('P$4ssword',10)
-
-  for (let i = 0; i <= activeUserCount + inActiveUserCount; i++) {
-    const res = await User.create({
-      username: `user${i+1}`,
-      email: `user${i+1}@gmail.com`,
-      inactive: i >= activeUserCount,
-      password:hash
-    });
-  }
-};
-
 
 // Not to use in prod: data may loose
-sequelize.sync({force:true}).then(async()=>{
-  await addUsers(9)
-})
+sequelize.sync().then(async () => {
+  await addUsers(9);
+});
 
-
-TokenService.scheduleCleanup()
+TokenService.scheduleCleanup();
 
 app.listen(3000, () => {
   console.log(`"Server·is·running·on·PORT·3000"`);
